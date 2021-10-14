274bede89f36644867dca93a2ff16e16dc9fd7b6
MIKE <17040066@itsuruapan.edu.mx>
2021-10-13 22:40:19 -0500

    Agregue el printl

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,11 +8,17 @@ public class Main {
         System.out.println("Modificacion hecha por Miguel");
 
         System.out.println("EL resultado de 5+3 es: " + suma(5,3));
+        System.out.println("2 y 2 son cuatro, y dos son 6, 2 son 8 y 8 son " + multi(4,4));
+
 
     }
 
     public static int suma(int n1, int n2){
         return n1+n2;
+    }
+
+    public static int multi(int n1, int n2){
+        return n1*n2;
     }
 
 }
