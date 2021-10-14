9ad65cb95b99fd19b5b104d03ddc5daaf4bbd5d4
GustavoSebastianMolina <17040095@itsuruapan.edu.mx>
2021-10-13 22:36:33 -0500

    Creación de una clase suma

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hola mundo Gustav1");
+
+        System.out.println("Hola mundo Gustav1");
         System.out.println("Modificacion hecha por Miguel");
 
+        System.out.println("EL resultado de 5+3 es: " + suma(5,3));
+
+    }
+
+    public static int suma(int n1, int n2){
+        return n1+n2;
     }
 
 }
