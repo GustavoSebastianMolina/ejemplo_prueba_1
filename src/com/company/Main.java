c9078fe0f2def371e0cfb0e317879826061f0e98
MIKE <17040066@itsuruapan.edu.mx>
2021-10-13 22:33:00 -0500

    Agregue el printl

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hola mundo Gustav1");
+        System.out.println("Modificacion hecha por Miguel");
+
     }
+
 }
